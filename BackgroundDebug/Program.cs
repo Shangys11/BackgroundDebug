c55a88756e2b0d4b156c9f2236a1ccf25e855f1b
Estrella Reyes, Santiago <SEstrella@livingston.com>
2025-08-14 12:59:19 -0600

    Add project files.

---
+++ BackgroundDebug/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSMxCore.Background;
+using TSMxCore.Background.Reports;
+using TSMxCore.Persistence;
+using TSMxCore.Repositorys;
+using TSMxCore.Service;
+using TSMxCore.Tools;
+
+namespace BackgroundDebug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var emp = 13;
+            var user = "gmedina";
+            var connection = "Data Source=LTPF4GSQ4A\\MSSQLSERVER2019;Initial Catalog=TSMX_DEMO;Integrated Security=SSPI;Persist Security Info=True;";
+            var tsmxContext = new TSMxContext(emp, user, connection);
+            var unitOfWork = new UnitOfWork(tsmxContext);
+            RepositoryCollection repos = new RepositoryCollection();
+            RepositoryService.Initialize(repos, tsmxContext);
+
+            var query = new CheckBoxParametrosQuery {facturaId = 6701929};
+            string filename = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".json");
+            JSONSerializer.SerializerJSONToFile(filename, query);
+
+            VanguardiaBackground vanguardiaBackground = new VanguardiaBackground(tsmxContext,unitOfWork, repos, 5);
+            vanguardiaBackground.Run(new string[] { "VANGUARDIA_TXT", emp.ToString(),user,  filename});
+        }
+
+    }
+}
---
+++ BackgroundDebug/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BackgroundDebug")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Livingston International")]
+[assembly: AssemblyProduct("BackgroundDebug")]
+[assembly: AssemblyCopyright("Copyright © Livingston International 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("155e74c0-9091-4c2e-897c-dd5f75247990")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
